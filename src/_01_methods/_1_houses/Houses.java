339f3b6c1f12f1f419efce81c4c33a67c2037876
league <league@imac-24.attlocal.net>
2023-08-23 16:55:12 -0700

    houses

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -1,4 +1,7 @@
 package _01_methods._1_houses;
+
+import org.jointheleague.graphical.robot.Robot;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2021
  *    Level 1
@@ -7,5 +10,24 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
+		Robot r = new Robot();
+		r.setX(25);
+		r.setY(50);
+		
+		house(10);
+		} 
+	 void house(int a) {
+		Robot r = new Robot();
+		int height=100;
+		r.penDown();
+		r.move(height);
+		r.turn(90);
+		r.move(25);
+		r.turn(90);
+		r.move(height);
+		r.penUp();
+		r.turn(-90);
+		r.penDown();
+		r.move(15);
 	}
 }
