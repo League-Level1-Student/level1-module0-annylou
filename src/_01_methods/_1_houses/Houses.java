8d63291abe859ae2bc46eb9fea6bc9c3a75803cc
league <league@imac-24.attlocal.net>
2023-08-30 16:53:49 -0700

    houses

--- src/_01_methods/_1_houses/Houses.java
+++ src/_01_methods/_1_houses/Houses.java
@@ -10,24 +10,59 @@ public class Houses {
 		
 	public void run() {
 		// Check the recipe to find out what code to put here
-		Robot r = new Robot();
-		r.setX(25);
-		r.setY(50);
+				
+		house();
 		
-		house(10);
 		} 
-	 void house(int a) {
+	 void house() {
 		Robot r = new Robot();
-		int height=100;
+		r.miniaturize();
+		r.setX(50);
+		r.setY(250);
+		r.setSpeed(10);
+		r.setRandomPenColor();
 		r.penDown();
-		r.move(height);
-		r.turn(90);
-		r.move(25);
-		r.turn(90);
-		r.move(height);
+		r.move(100);
+		 r.penUp();
+		 r.turn(90);
+		 r.move(29);
+		 flat();
+		 r.turn(90);
+		 r.penDown();
+		r.move(100);
 		r.penUp();
 		r.turn(-90);
+		r.setPenColor(12,143,7);
 		r.penDown();
-		r.move(15);
-	}
+		r.move(20);
+	 }
+	 
+	 void pointy() {
+		 Robot r = new Robot();
+		 r.penDown();
+		 r.setRandomPenColor();
+		 r.hide();
+		 r.setX(50);
+		 r.setY(150);
+		 r.setSpeed(10);
+		 r.turn(45);
+		 r.move(20);
+		 r.turn(90);
+		 r.move(20);
+		 r.turn(45);
+	 }
+	 
+	 void flat() {
+		 Robot r = new Robot();
+		 r.setRandomPenColor();
+		 r.penDown();
+		 r.hide();
+		 r.setX(50);
+		 r.setY(150);
+		 r.setSpeed(10);
+		 r.turn(90);
+		 r.move(29);
+		 r.turn(90);
+	 }
 }
+
